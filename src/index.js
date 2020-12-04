5575c5c55605520b7dd5266b482870ba7de19ee7
Jennifer Lucas <36136983+jclucas@users.noreply.github.com>
2020-12-04 16:21:07 -0500

    add informative tooltip

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ import Game from "./js/Game.js"
 
 var game = new Game();
 game.loop();
+
+// disable tooltip on first click
+let tip = document.getElementById("tip");
+let handler = () => {
+    tip.style.display = "none";
+    document.removeEventListener('mousedown', handler);
+};
+
+document.addEventListener('mousedown', handler);
